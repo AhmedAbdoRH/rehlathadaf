63b3b9f0b1de737a5c27aeeae9112c542cbcbc75
Rehlat E-Commerce <rehlathadaf@gmail.com>
2025-10-27 04:14:30 +0000

    لا لا تنشئ مشروع جديد عرفني المشروع المسجل بالفعل لربطه مجددا لاني لا ار

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,7 +2,6 @@ import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { firebaseConfig } from './firebase-config';
 
-
 // Initialize Firebase
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
 const db = getFirestore(app);
